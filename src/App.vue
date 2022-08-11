2db945af623ec9f3ae5ef1acdf436270c6a2357c
kema-dev <54537427+kema-dev@users.noreply.github.com>
2022-08-11 18:34:20 +0200

    trying to used custom font

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,92 @@
 <template>
-  <div class="top">
-    <h1 class="title">kemadev</h1>
-    <p class ="subtitle">Improve your efficiency, keep your privacy</p>
-  </div>
+  <component :is="'script'">
+    <script src="https://kit.fontawesome.com/cefbadd795.js" crossorigin="anonymous"></script>
+  </component>
+	<div class="top">
+		<div class="top-inside">
+      <h1>kema-dev</h1>
+      <h3>Improve your efficiency, keep your privacy</h3>
+    </div>
+	</div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-  }
-}
+	name: "App",
+	components: {},
+	data() {
+		return {
+		};
+	},
+};
 </script>
 
 <style lang="css">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: white;
 }
 
 .top {
-  height: 60vh;
+	position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 65vh;
+}
+.top::before {
+	content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 65vh;
   background-image: url('./assets/img/data_flow_2_full.jpg');
+  background-size: cover;
+  filter: opacity(70%);
+  z-index: -1;
+}
+.top-inside {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 65vh;
 }
 
-.title {
-  /* margin-top: 10vh; */
+h1, h2, h3, h4, h5, h6 {
+  margin: 0;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+
+h1 {
+  font-size: 3.5rem;
+  font-weight: 300;
+  margin-bottom: 2rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+  font-weight: 300;
+  margin-bottom: 0.5rem;
 }
 
 div {
-  margin: 0;
+	margin: 0;
 }
 
-html, body {
-  margin: 0;
-  padding: 0;
+html,
+body {
+	background-color: #555555;
+	margin: 0;
+	padding: 0;
 }
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faPhone);
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+createApp(App)
+	.component("font-awesome-icon", FontAwesomeIcon)
+	.mount('#app')
